96573258449847e4d0b27ac4546825dc1615f779
Brandon Ly <bl4720@rit.edu>
2020-09-14 17:17:47 -0700

    Helper functions imported and one error on ctx

--- project1/src/blLIB.js
+++ project1/src/blLIB.js
@@ -1,4 +1,36 @@
+console.log("blLIB loaded");
 (function () {
     "use strict";
+    let blLIB = {
+        getRandomColor() {
+            const getByte = _ => 55 + Math.round(Math.random() * 200);
+            return `rgba(${getByte()},${getByte()},${getByte()},.9)`;
+        },
 
+        getRandomInt(min, max) {
+            return Math.floor(Math.random() * (max - min + 1)) + min;
+        },
+
+        drawRectangle(ctx, x, y, width, height, fillStyle = "black", lineWidth = 0, strokeStyle = "black") {
+            ctx.fillStyle = fillStyle;
+            ctx.save();
+            ctx.beginPath();
+            ctx.rect(x, y, width, height);
+            ctx.closePath();
+            ctx.fill();
+            if (lineWidth > 0) {
+                ctx.lineWidth = lineWidth;
+                ctx.strokeStyle = strokeStyle;
+                ctx.stroke();
+            }
+            ctx.restore();
+        }
+    };
+
+    if (window) {
+        window["blLIB"] = blLIB
+    }
+    else {
+        throw "'window' is not defined!";
+    }
 })();
--- project1/src/index.js
+++ project1/src/index.js
@@ -1,4 +1,20 @@
+console.log("index.js loaded");
 (function () {
     "use strict";
+	const canvasWidth = 400, canvasHeight = 300;
+	let ctx;
 
+	window.onload = init;
+
+	function init() {
+		ctx = canvas.getContext("2d");
+		canvas.width = canvasWidth;
+		canvas.height = canvasHeight;
+		ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+		loop();
+	}
+
+	function loop() {
+		
+	}
 })();
